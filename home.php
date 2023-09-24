790f42ae1e335a100e175c5eb4e19060a4555667
GreatCode1989 <wef666wef6@gmail.com>
2023-09-24 15:54:42 +0300

    lo

--- home.php
+++ home.php
@@ -12,91 +12,48 @@ Template Name: home
     <main class="main">
       <section class="info">
         <div class="container">
-          <div class="grid-item">
-            <img class="img-grid" src="<?php echo bloginfo('template_url'); ?>/assets/image/program.png" alt="program" />
+         
+
+          <?php
+global $post;
+
+$myposts = get_posts([ 
+	'numberposts' => -1,
+]);
+
+if( $myposts ){
+	foreach( $myposts as $post ){
+		setup_postdata( $post );
+		?>
+	
+  <div class="grid-item">
+          
+            <?php the_post_thumbnail(
+              array(346, 192),
+              array(
+                'class' => 'img-grid'
+              )
+            ); ?>
             <div class="text">
-              <h2 class="title">Программа</h2>
+              <h2 class="title"><?php the_title(); ?></h2>
               <p class="text-p">
-                Мастер-классы от
-                <span class="text-s">звeзд украинского шоу-бизнеса</span>
-              </p>
-              <button class="btn-grid">
-                <img src="<?php echo bloginfo('template_url'); ?>/assets/image/grid-1.png" alt="microphone" />
-              </button>
-            </div>
-          </div>
-          <div class="grid-item">
-            <img class="img-grid" src="<?php echo bloginfo('template_url'); ?>/assets/image/fire.png" alt="fire" />
-            <div class="text">
-              <h2 class="title">Развлечения</h2>
-              <p class="text-p">
-                Бассейн с подогревом, собственный
-                <span class="text-s">
-                  надувной аквапарк, пляжный кинотеатр</span
-                >
-              </p>
-              <button class="btn-grid">
-                <img src="<?php echo bloginfo('template_url'); ?>/assets/image/grid-2.png" alt="microphone" />
-              </button>
-            </div>
-          </div>
-          <div class="grid-item">
-            <img class="img-grid" src="<?php echo bloginfo('template_url'); ?>/assets/image/boat.png" alt="boat" />
-            <div class="text">
-              <h2 class="title">Расположение</h2>
-              <p class="text-p">
-                1-я линия,
-                <span class="text-s">собственный песчаный пляж </span> на берегу
-                Черного моря
+                <?php the_content(); ?>
               </p>
               <button class="btn-grid">
                 <img src="<?php echo bloginfo('template_url'); ?>/assets/image/grid-3.png" alt="microphone" />
               </button>
             </div>
           </div>
-          <div class="grid-item">
-            <img
-              class="img-grid"
-              src="<?php echo bloginfo('template_url'); ?>/assets/image/vegetables.png"
-              alt="vegetables.png"
-            />
-            <div class="text">
-              <h2 class="title">Питание</h2>
-              <p class="text-p">
-                <span class="text-s">Шведская линия</span> с пятиразовым
-                питанием от шеф-повара
-              </p>
-              <button class="btn-grid">
-                <img src="<?php echo bloginfo('template_url'); ?>/assets/image/grid-4.png" alt="microphone" />
-              </button>
-            </div>
-          </div>
-          <div class="grid-item">
-            <img class="img-grid" src="<?php echo bloginfo('template_url'); ?>/assets/image/residence.png" alt="residence" />
-            <div class="text">
-              <h2 class="title">Проживание</h2>
-              <p class="text-p">
-                Современные кондиционированные
-                <span class="text-s"><br />4-х местные</span> номера
-              </p>
-              <button class="btn-grid">
-                <img src="<?php echo bloginfo('template_url'); ?>/assets/image/grid-5.png" alt="microphone" />
-              </button>
-            </div>
-          </div>
-          <div class="grid-item">
-            <img class="img-grid" src="<?php echo bloginfo('template_url'); ?>/assets/image/safety.png" alt="safety" />
-            <div class="text">
-              <h2 class="title">Безопасность</h2>
-              <p class="text-p">
-                Охраняемая территория 24/7
-                <span class="text-s">(157 камер видеонаблюдения)</span>
-              </p>
-              <button class="btn-grid">
-                <img src="<?php echo bloginfo('template_url'); ?>/assets/image/grid-6.png" alt="microphone" />
-              </button>
-            </div>
-          </div>
+
+		<?php 
+	}
+}
+
+wp_reset_postdata(); 
+?>
+           
+         
+        
         </div>
       </section>
       <section class="more-info">
